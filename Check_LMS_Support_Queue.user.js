f63b5ccb04201a419911db4309fde59efd9d1887
tidusx18 <victoriano518@gmail.com>
2020-03-02 17:26:51 -0500

    Fix: Missing self-executing function open/close parentheses.

--- Check_LMS_Support_Queue.user.js
+++ Check_LMS_Support_Queue.user.js
@@ -2,7 +2,7 @@
 // @name         Check SF LMS Support Queue
 // @namespace    http://github.com/tidusx18
 // @updateURL    https://github.com/tidusx18/salesforce-notifications/raw/master/Check_LMS_Support_Queue.user.js
-// @version      0.0.3
+// @version      0.0.4
 // @author       Daniel Victoriano <victoriano518@gmail.com>
 // @match        https://fiuonline.lightning.force.com/lightning/*
 // @run-at       default
@@ -105,4 +105,4 @@
             .then( res => res.context.globalValueProviders[5] ? dispatchNotification() : console.log('Nothing in queue...') )
 
     }, 600000) // time is in milliseconds (1000 == 1 second)
-})
+})()
